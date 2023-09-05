3214da649b5ef75331193707ea3e3098f1f1244d
anshimathur0325 <anshimathur0325@gmail.com>
2023-09-05 17:55:08 -0500

    Added Bullet Points

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,13 @@ export default function Projects(){
     return(
         <section id="projects">
             <body class='proj'>
-                <p>One project I worked on is a Flask application to predict if someone has a chance of getting diabetes.</p>
+                <h3>
+                    Personal Projects
+                </h3>
+                <ul>
+                <li>Created a machine learning application using Flask to predict a person's risk for diabetes.</li>
+                <li>Posted article on LinkedIn about lifestyles that increase risk of brain stroke.</li>
+                </ul>
             </body>
         </section>
     )
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,7 +4,10 @@ export default function Skills(){
     return(
         <section id="projects">
             <body class='proj2'>
-                <p>Skills: Java, Python, HTML/CSS/JS</p>
+                <h3>Skills</h3>
+                <ul>
+                    <li>Java, Python, HTML/CSS/JS, React.js, SQL</li>
+                </ul>
             </body>
         </section>
     )
