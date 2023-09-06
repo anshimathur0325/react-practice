e0cdd195ebc002edfb03672a58b7dffab6d39dd5
anshimathur0325 <anshimathur0325@gmail.com>
2023-09-06 16:20:19 -0500

    Edit navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,14 @@ import React from "react";
 export default function Navbar(){
     return(
         <section id="navbar">
-            
             <body class='nav'>
-                    <nav>
-                        <a href="https://anshimathur.netlify.app/">Older Page</a>
-                        <a href="https://www.linkedin.com/in/anshi-mathur-770688236/">LinkedIn</a>
-                    </nav>
+                <nav>
+                    <a href="mailto:anshimathur0325@gmail.com"><img src = {require("./images/mail.png")}/></a>
+                    <a href="https://www.linkedin.com/in/anshi-mathur-770688236/"><img src = {require("./images/linkedin.png")}/></a>
+                    <a href="https://github.com/anshimathur0325"><img src = {require("./images/github.png")}/></a>    
+                    
+                </nav>
             </body>
-            
         </section>
     )
 }
