303a7e054888996cfa8634d434be4914eacc1936
anshimathur0325 <anshimathur0325@gmail.com>
2023-09-08 15:06:24 -0500

    edit website layout

--- src/components/MoreAbout.js
+++ src/components/MoreAbout.js
@@ -5,7 +5,7 @@ export default function MoreAbout(){
         <section id="moreabout">
             <body class="proj4">
                 <h3>About Me</h3>
-                <p>Hi! My name is Anshi Mathur and I am currently a freshman at the University of Illinois at Urbana-Champaign! This website is made from React, which I am currently learning.</p>
+                <p>Hi! My name is Anshi Mathur and I am currently a freshman at the University of Illinois at Urbana-Champaign! I made this website in its entirety with React, which I am currently learning.</p>
                 <p>I am currently looking for internships, so please feel free to reach out with any questions!</p>
             </body>
             
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,9 +8,13 @@ export default function Projects(){
                 </h3>
                 <ul>
                 <li>Created a machine learning application using Flask to predict a person's risk for diabetes.</li>
+                </ul>
+                <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:6983966954004914177" height="100%" width="100%" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
                 <br/>
+                <ul>
                 <li>Posted article on LinkedIn about lifestyles that increase risk of brain stroke.</li>
                 </ul>
+                <p><iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6981465057809358848" height="100%" width="100%" frameborder="0" allowfullscreen="" title="Embedded post"></iframe></p>
             </body>
         </section>
     )
