3edd90590e87c1116feb2f1aec33dac0ab846961
anshimathur0325 <anshimathur0325@gmail.com>
2023-09-10 22:17:38 -0500

    edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,7 @@ export default function Footer(){
             <footer>
             <h2>Please Contact Me</h2>
             <div className="footer__socials">
+            <a href="mailto:anshimathur0325@gmail.com">Email</a>    
             <a href="https://www.linkedin.com/in/anshi-mathur-770688236" target="_blank" rel="noreferrer" >LinkedIn</a>
             <a href="https://github.com/anshimathur0325" target="_blank" rel="noreferrer" >Github</a>
             </div>
