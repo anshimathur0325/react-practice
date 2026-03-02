c55e9e0c35aa06287834472c9a397b9ba58df126
anshimathur0325 <anshimathur0325@gmail.com>
2026-03-01 18:33:19 -0600

    update aboutme

--- src/components/MoreAbout.js
+++ src/components/MoreAbout.js
@@ -9,7 +9,7 @@ const MoreAbout = forwardRef((props, ref) => {
 
       <body className="MoreAbout">
         <h3>About Me</h3>
-        <p>Hi! My name is Anshi Mathur and I am currently a Sophomore at the University of Illinois at Urbana-Champaign! I am also currently a Data Science co-op at Bayer!!</p>
+        <p>Hi! My name is Anshi Mathur and I am currently a Senior at the University of Illinois at Urbana-Champaign! I am also currently a Data Science intern at Bayer!!</p>
         <p>I am always looking for opportunities to grow, so please feel free to reach out with any questions!</p>
       </body>
     </section>
